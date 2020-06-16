fa5249377af84ce0b90e81938319ab545de7f153
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-06-16 10:04:21 +0900

    초기 커밋.

---
+++ SchoolTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SchoolTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SchoolTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8588cc11-4014-4c95-ad26-2aabfc16d944")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SchoolTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using School;
+
+namespace SchoolTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<Teacher> Teachers = new List<Teacher>();
+            Program.setTeacherList(Teachers);
+
+            Assert.AreEqual("함기훈", Teachers[0].name, "실패");
+        }
+
+        [TestMethod]
+        public void T01_add_major()
+        {
+            Student student = new Student("3117", "이지아", new DateTime(2002, 07, 07));
+            student.addMajor(0, "정보컴퓨터");
+        }
+
+        
+    }
+}
